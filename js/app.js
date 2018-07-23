d0bf371ecfc5e45bfb0bdaa437c57b500030ad82
sandi87 <sndrstank@gmail.com>
2018-07-23 17:57:46 +0200

    transform code to es6 standard and made bacground of the game seen

--- js/app.js
+++ js/app.js
@@ -1,35 +1,52 @@
 // Enemies our player must avoid
-var Enemy = function() {
+class Enemy {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+constructor(x,y, speed)
+    {this.sprite = 'images/enemy-bug.png';}
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+update(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
 
+}
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 };
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+  constructor(x,y) {
+    this.sprite = 'images/char-cat-girl.png'
+  }
+  update() {
 
+
+  }
+  render() {
+
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+  handleInput() {
+
+  }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+const allEnemies = [];
+const player = new Player(10, 10);
 
 
 // This listens for key presses and sends the keys to your
